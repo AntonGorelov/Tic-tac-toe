42bbdd5175a0bc3147060ef2ee3979530404bf55
Kirill Mashtalov <kirill.n28@gmail.com>
2015-12-05 21:00:19 +0300

    -добавлена функция печати элемента

--- TicTacToe.hs
+++ TicTacToe.hs
@@ -4,3 +4,9 @@ data Result = First | Draw | Second
   deriving (Eq, Show)
   
 type Field = [[Int]]
+
+printElem :: Int -> IO ()
+printElem 0 = putStr "-"
+printElem 1 = putStr "X"
+printElem 2 = putStr "0"
+printElem _ = error "incorrect move"
