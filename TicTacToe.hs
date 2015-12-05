346ff8772aed04c3b6de9249895085636d15e625
AntonGorelov <ant7016@yandex.ru>
2015-12-05 22:05:50 +0300

    Добавлена функция печати поля

--- TicTacToe.hs
+++ TicTacToe.hs
@@ -10,3 +10,20 @@ printElem 0 = putStr "-"
 printElem 1 = putStr "X"
 printElem 2 = putStr "0"
 printElem _ = error "incorrect move"
+
+printString :: [Int] -> IO ()
+printString (x:y:z:[]) = do
+  printElem x
+  printElem y
+  printElem z
+printString _ = error "incorrect string"
+  
+printField :: Field -> IO ()
+printField (x:y:z:[]) = do
+  printString x
+  putStrLn ""
+  printString y
+  putStrLn ""
+  printString z
+  putStrLn ""
+printField _ = error "incorrect field"
