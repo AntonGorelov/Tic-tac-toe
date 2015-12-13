9f8882c85e645c18d22d046e6a9b5162b1af7ce4
Kirill Mashtalov <kirill.n28@gmail.com>
2015-12-13 13:53:49 +0300

    Добавлено выполнение хода

--- TicTacToe.hs
+++ TicTacToe.hs
@@ -27,3 +27,16 @@ printField (x:y:z:[]) = do
   printString z
   putStrLn ""
 printField _ = error "incorrect field"
+
+move :: Int -> Int -> Field -> Field
+move n p [[a,b,c], [d,e,f], [g,h,i]]
+  | n == 1 && a == 0 = [[p,b,c], [d,e,f], [g,h,i]]
+  | n == 2 && b == 0 = [[a,p,c], [d,e,f], [g,h,i]]
+  | n == 3 && c == 0 = [[a,b,p], [d,e,f], [g,h,i]]
+  | n == 4 && d == 0 = [[a,b,c], [p,e,f], [g,h,i]]
+  | n == 5 && e == 0 = [[a,b,c], [d,p,f], [g,h,i]]
+  | n == 6 && f == 0 = [[a,b,c], [d,e,p], [g,h,i]]
+  | n == 7 && g == 0 = [[a,b,c], [d,e,f], [p,h,i]]
+  | n == 8 && h == 0 = [[a,b,c], [d,e,f], [g,p,i]]
+  | n == 9 && i == 0 = [[a,b,c], [d,e,f], [g,h,p]]
+  | otherwise = error "incorrect move"
